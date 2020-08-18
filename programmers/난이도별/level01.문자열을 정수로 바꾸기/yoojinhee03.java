f3d9be9303688f24fb81b1109a5cdc99809330d0
jinhee Yoo <66635648+yoojinhee03@users.noreply.github.com>
2020-08-18 20:52:57 +0900

    Create level01.문자열을 정수로 바꾸기/yoojinhee03.java

---
+++ programmers/난이도별/level01.문자열을 정수로 바꾸기/yoojinhee03.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class yoojinhee03 {
+
+	public static void main(String[] args) {
+		System.out.println(solution("-1234"));
+		
+	}
+	public static int solution(String s) {
+		return Integer.parseInt(s);
+    }
+}
+	
