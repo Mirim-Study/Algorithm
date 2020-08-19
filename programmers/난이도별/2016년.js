edf68ac3a8a0af93b35b27295d2abe2705a81a26
Areum Yang <s2018s10@e-mirim.hs.kr>
2020-08-19 14:21:33 +0900

    Delete 2016년.js

--- programmers/난이도별/2016년.js
+++
@@ -1,7 +0,0 @@
-function solution(a, b) {
-    var answer = '';
-    const date = new Date(`2016, ${a},${b}`);
-    answer = (date+"").substring(0,3);
-    answer = answer.toUpperCase();
-    return answer;
-}
