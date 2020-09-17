d569a4ed2638ad6315a193e94bf71c4cf8570651
Soyoung, Park <48789583+SSSOy@users.noreply.github.com>
2020-09-17 12:18:12 +0900

    Create SSSOy.java

---
+++ programmers/level01.평균_구하기/SSSOy.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i : arr) 
+            answer += i;
+        
+        return answer / arr.length;
+    }
+}
