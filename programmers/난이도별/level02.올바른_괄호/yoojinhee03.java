5d2240147ff55da90b03c1b4c8cc909889b600bb
jinhee Yoo <66635648+yoojinhee03@users.noreply.github.com>
2020-12-23 13:38:55 +0900

    Update programmers/난이도별/level02.올바른_괄호/yoojinhee03.java
    삼항연산자로 변경

--- programmers/난이도별/level02.올바른_괄호/yoojinhee03.java
+++ programmers/난이도별/level02.올바른_괄호/yoojinhee03.java
@@ -4,11 +4,7 @@ class Solution {
         int n=0;
         for(int i=0; i<s.length(); i++) {
             char c=s.charAt(i);
-            if(c=='(') {
-                n++;
-            }else {
-                n--;
-            }
+n=c==('(')?n+1:n-1;
             if(i==s.length()-1&&n!=0||n<0) {
                 return false;
             }
