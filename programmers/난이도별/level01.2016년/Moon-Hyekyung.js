f3da2f6ec6517be6ad881485f2cba52d9d02f876
Moon-HyeKyung <75727995+Moon-HyeKyung@users.noreply.github.com>
2021-09-09 23:49:44 +0900

    Create Moon-Hyekyung.js

---
+++ programmers/난이도별/level01.2016년/Moon-Hyekyung.js
@@ -0,0 +1,6 @@
+function get2016Day(a, b) {
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+    const dayIdx = new Date(`2016-${a}-${b}`).getDay()
+
+    return days[dayIdx];
+}
