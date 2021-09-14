d0c79ae7434fc88e282b0621de5d7eca3fc94836
Moon-HyeKyung <75727995+Moon-HyeKyung@users.noreply.github.com>
2021-09-14 23:18:52 +0900

    fix: add semicolon

--- programmers/난이도별/level01.2016년/Moon-Hyekyung.js
+++ programmers/난이도별/level01.2016년/Moon-Hyekyung.js
@@ -1,6 +1,6 @@
 function solution(a, b) {
-    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-    const dayIdx = new Date(`2016-${a}-${b}`).getDay()
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const dayIdx = new Date(`2016-${a}-${b}`).getDay();
     
     return days[dayIdx];
 }
