413ec361df2c97ac7e26c8255b8872854726507d
Moon-HyeKyung <75727995+Moon-HyeKyung@users.noreply.github.com>
2021-09-15 23:16:48 +0900

    fix: eliminate a variable unused

--- programmers/난이도별/level01.2016년/Moon-Hyekyung.js
+++ programmers/난이도별/level01.2016년/Moon-Hyekyung.js
@@ -1,6 +1,5 @@
 function solution(a, b) {
     const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const dayIdx = new Date(`2016-${a}-${b}`).getDay();
     
-    return days[dayIdx];
+    return days[new Date(`2016-${a}-${b}`).getDay()];
 }
