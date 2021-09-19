494ee998edecf5a4ea8d3198b741936f1dffa39a
이기쁨 Gippeum Lee <s2018s13@e-mirim.hs.kr>
2021-09-20 02:28:06 +0900

    Create pppeum62.js

---
+++ programmers/난이도별/level01.소수_만들기/pppeum62.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+    let answer = 0;
+    const isPrime = num => {
+        for (let i = 2; i < num; i++)
+            if (num % i === 0) return false;
+        return num > 1;
+    }
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let temp = 0;
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                temp = nums[i] + nums[j] + nums[k];
+                if (isPrime(temp)) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
