8f0ccd8a1a0407599e6e162ec0da18a19a989381
Soyoung, Park <thdud00627@gmail.com>
2021-09-20 15:18:48 +0900

    Create SSSOy.java

---
+++ programmers/난이도별/level02.최솟값_만들기/SSSOy.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = 0; i < A.length; i++) 
+            answer += A[i] * B[B.length - (i + 1)];
+
+        return answer;
+    }
+}
