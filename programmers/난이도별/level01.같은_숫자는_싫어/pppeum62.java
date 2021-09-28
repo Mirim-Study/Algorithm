a29565cf3ac7c3d4ab7d9c799075106c433c9327
이기쁨 Gippeum Lee <s2018s13@e-mirim.hs.kr>
2021-09-28 21:35:57 +0900

    Update pppeum62.java

--- programmers/난이도별/level01.같은_숫자는_싫어/pppeum62.java
+++ programmers/난이도별/level01.같은_숫자는_싫어/pppeum62.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Solution {
     public List<Integer> solution(int []arr) {
-        List<Integer> answer = new ArrayList<Integer>(List.of(arr[0]));
+        final List<Integer> answer = new ArrayList<Integer>(List.of(arr[0]));
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] != arr[i - 1])
