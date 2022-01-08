9e67cf17e10aa3d59b19a9ec5c33b29f802c680e
Y2NY <43701352+y2ny@users.noreply.github.com>
2022-01-09 01:59:12 +0900

    Update y2ny.js

--- programmers/난이도별/level01.없는_숫자_더하기/y2ny.js
+++ programmers/난이도별/level01.없는_숫자_더하기/y2ny.js
@@ -1,6 +1,3 @@
 function solution(numbers) {
-    let allNumberSum = 45;
-    let numbersSum = numbers.reduce((a, b) => a + b);
-    
-    return allNumberSum - numbersSum;
+    return 45 - numbers.reduce((a, b) => a + b);
 }
