9e5079fc8cd3d9bd6222a8c9585c1adacce7492f
Moon-HyeKyung <75727995+Moon-HyeKyung@users.noreply.github.com>
2022-02-01 15:39:47 +0900

    Create Moon-Hyekyung.js

---
+++ programmers/난이도별/level01.부족한_금액_계산하기/Moon-Hyekyung.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+    let sum = 0; 
+    for(let index = 1; index <=count; index++ ) {
+        sum += price * index;
+    }
+    
+    return sum - money <= 0 ? 0 : sum - money;
+}
