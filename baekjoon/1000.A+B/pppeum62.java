ce9fb234f7f014c0ca5efab728657efc7a57b468
이기쁨 Gippeum Lee <im.thisjoy@gmail.com>
2022-03-21 22:46:05 +0900

    Create pppeum62.java

---
+++ baekjoon/1000.A+B/pppeum62.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String ar[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a+b);
+        
+        sc.close();
+    }
+}
