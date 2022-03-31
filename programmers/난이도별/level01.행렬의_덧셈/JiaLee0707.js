a66648b5b5686b4927a7b293fef2b30429e9d4dc
Jia Lee <44323898+JiaLee0707@users.noreply.github.com>
2022-03-31 23:36:08 +0900

    Update JiaLee0707.js

--- programmers/난이도별/level01.행렬의_덧셈/JiaLee0707.js
+++ programmers/난이도별/level01.행렬의_덧셈/JiaLee0707.js
@@ -1,3 +1,4 @@
+// 1
 function solution(arr1, arr2) {
     if (!Array.isArray(arr1)) return arr1 + arr2;
     
@@ -9,3 +10,20 @@ function solution(arr1, arr2) {
     
     return temp;
 }
+
+// 2
+// function solution(arr1, arr2) {
+//     let answer = [];
+//     const arrLength = arr1.length;
+    
+//     for(let i=0; i<arrLength; i++) {
+//         answer.push([]);
+//         const arrLength2 = arr1[i].length;
+        
+//         for(let j=0; j<arrLength2; j++) {
+//             answer[i].push(arr1[i][j] + arr2[i][j]);
+//         }
+//     }
+    
+//     return answer;
+// }
