219315c64416f007914ee4968ae965444e64e73f
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-06-24 12:28:42 +0900

    map을 활용하여 수정

--- leetcode/Stack/20.Valid_Parentheses/areumsheep.js
+++ leetcode/Stack/20.Valid_Parentheses/areumsheep.js
@@ -3,31 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // 1. stack 배열 만들기 (안에 아무것도 없는 상태)
-    // 2. s를 array로 바꾸기 변수명은 brackets
-    // 3. s의 길이만큼 for문 돌기
-    // 4. 만약 (, { , [ 가 나오면 stack에 넣기
-    // 5. 만약 ), 앞에 ( 가 있다면 stack에 있는 ( 문자 꺼내기
-    // 6. 만약 }, 앞에 { 가 있다면 stack에 있는 { 문자 꺼내기
-    // 7. 만약 ], 앞에 [ 가 있다면 stack에 있는 [ 문자 꺼내기
-    // 8. for문이 끝나고 stack 변수가 비어있다면 true, 아니면 false
+    if(s.length % 2 !== 0) return false;
     
     const stack = [];
-    const brackets = s.split('');
+    const map = new Map([
+        ['(', ')'],
+        ['[', ']'],
+        ['{', '}']
+    ]);
     
-    for(let bracket of brackets) {
-        const top = stack[stack.length - 1]
-        if(bracket === '(' || bracket === '{' || bracket === '[')
-            stack.push(bracket);
-        else if (bracket === ')' && top === '(')
-            stack.pop();
-        else if (bracket === '}' && top === '{')
-            stack.pop();
-        else if (bracket === ']' && top === '[')
-            stack.pop();
-        else return false;
+    for(let i = 0; i < s.length; i++) {
+        if(map.has(s[i])) {
+            stack.push(map.get(s[i]));
+        } else if(s[i] !== stack.pop()){
+            return false;
+        }
     }
     
-    if(stack.length === 0) return true;
-    return false;
+    
+    return stack.length === 0;
 };
