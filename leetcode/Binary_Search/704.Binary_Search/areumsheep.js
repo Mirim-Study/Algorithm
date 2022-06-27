8d4f69658a6eb1191d73943a95d306a533b611c2
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-06-27 16:39:04 +0900

    Update areumsheep.js

--- leetcode/Binary_Search/704.Binary_Search/areumsheep.js
+++ leetcode/Binary_Search/704.Binary_Search/areumsheep.js
@@ -4,9 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i = 0; i<nums.length; i++) {
-        if(nums[i] === target)
-            return i;
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor(left + (right - left) / 2)
+        
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
+    
     return -1;
 };
