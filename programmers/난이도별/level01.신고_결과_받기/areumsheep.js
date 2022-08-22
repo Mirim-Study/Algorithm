c70fafcd330de55831c5c58644f83eb18d3e94d6
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-08-22 16:58:33 +0900

    Update: 주석 추가

--- programmers/난이도별/level01.신고_결과_받기/areumsheep.js
+++ programmers/난이도별/level01.신고_결과_받기/areumsheep.js
@@ -1,25 +1,29 @@
 function solution(id_list, report, k) {
-    // 각 id 당 신고당한 횟수 reportedCount, 각 id 당 신고한 사람 reportedBy
     
+    // -- 변수 및 상수 선언
     const MAX_REPORT_COUNT = k;
-    const reportSet = new Set(report);
+    const reportSet = new Set(report); //Set을 통해 중복된 신고는 단일 신고로 변경
     
+    //각 id 당 신고당한 횟수: reportedCount / 각 id 당 신고한 사람: reportedBy
     const mailCount = {};     // {"id": Number(count)}
     const reportedCount = {}; // {"id": Number(count)}
     const reportedBy = {};    // {"id": []}
     
+    // -- 객체 초기화
     id_list.forEach((id) => {
         mailCount[id] = 0;
         reportedCount[id] = 0;
         reportedBy[id] = [];
     });
     
+    // -- 신고당한 횟수, 신고한 아이디 분리
     reportSet.forEach((report) => {
         const [userID, reportedID] = report.split(' ');
         reportedCount[reportedID] += 1;
         reportedBy[reportedID].push(userID);
     });
     
+    // -- 정지된 계정을 찾고 정지된 계정을 신고한 사용자의 mailCount 값 증가
     for(const user of id_list){
         if(reportedCount[user] >= MAX_REPORT_COUNT){
             reportedBy[user].forEach((reporter) => {
@@ -28,5 +32,6 @@ function solution(id_list, report, k) {
         }
     }
     
+    // -- 객체의 값만 뽑아서 리턴
     return Object.values(mailCount);
 }
