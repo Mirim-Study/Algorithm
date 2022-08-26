5fba207b28a47c2dbf43e5980aee8530602743a5
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-08-26 12:31:50 +0900

    Update areumsheep.js

--- programmers/난이도별/level01.성격_유형_검사하기/areumsheep.js
+++ programmers/난이도별/level01.성격_유형_검사하기/areumsheep.js
@@ -1,25 +1,30 @@
 function solution(survey, choices) {
-    var answer = '';
-    const surveyTypes = ['R','T','C','F','J','M','A','N'];
-    const choiceObj = {};
+    let answer = '';
+    const types = ['R','T','C','F','J','M','A','N'];
+    const score = {};
     
-    for(const type of surveyTypes) {
-        choiceObj[type] = 0;
+    const MAX_FIRST_TYPE = 3;
+    const MIDDLE_CHOICE = 4;
+    
+    for(const type of types) {
+        score[type] = 0;
     }
     
     for(const index in survey){
         const [first, second] = survey[index].slice('');
         const choice = choices[index];
         
-        if(choice < 4) choiceObj[first] += Math.round(3/choice);
-        else if (choice > 4) choiceObj[second] += choice - 4;
+        //비동의 (1~3)
+        if(choice < MIDDLE_CHOICE) score[first] += Math.round(MAX_FIRST_TYPE/choice);
+        //동의 (5~7)
+        else if (choice > MIDDLE_CHOICE) score[second] += choice - MIDDLE_CHOICE;
     }
     
     for(let index = 0; index <= 6; index += 2){
-        const firstType = choiceObj[surveyTypes[index]];
-        const secondType = choiceObj[surveyTypes[index+1]];
+        const firstType = score[types[index]];
+        const secondType = score[types[index+1]];
         
-        answer += firstType >= secondType ? surveyTypes[index] : surveyTypes[index+1];
+        answer += firstType >= secondType ? types[index] : types[index+1];
     }
     
     return answer;
