8e02da5deecb3015874136e828f77dad44712536
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-11-07 22:56:59 +0900

    Update: 주석 추가

--- goormedu/알고리즘{먼데이}/[2주차]-1.합격자_찾기/areumsheep.js
+++ goormedu/알고리즘{먼데이}/[2주차]-1.합격자_찾기/areumsheep.js
@@ -3,29 +3,34 @@ const readline = require('readline');
 const data = [];
 
 (async () => {
-	let rl = readline.createInterface({ input: process.stdin });
-	
-	for await (const line of rl) {
-		data.push(line);
-		rl.close();
-	}
-	
-	const doubleCount = data[0] * 2;
-	for(let i = 1; i < doubleCount; i+=2){
-		let successCount = 0;
-		const personCount = data[i];
-		const gradeArray = data[i+1].split(' ')
-											.filter((grade) => grade !== '')
-											.map(Number);
-		const gradeSum = gradeArray.reduce((acc, cur) => acc + cur);
-		
-		gradeArray.forEach((cur) => {
-			if(gradeSum / personCount <= cur) {
-				successCount++;
-			}
-		})
-		
-		console.log(`${successCount}/${personCount}`)
-	}
-	process.exit();
+  let rl = readline.createInterface({ input: process.stdin });
+
+  for await (const line of rl) {
+    data.push(line);
+    rl.close();
+  }
+
+  //총 라인 수 계산
+  const doubleCount = data[0] * 2;
+
+  //홀수번째 라인만 돌며 입력받은 점수 계산
+  for (let i = 1; i < doubleCount; i += 2) {
+    let successCount = 0;
+    const personCount = data[i];
+    const gradeArray = data[i + 1]
+      .split(' ')
+      .filter((grade) => grade !== '')
+      .map(Number);
+    const gradeSum = gradeArray.reduce((acc, cur) => acc + cur);
+
+    // 평균보다 높은 점수 카운트
+    gradeArray.forEach((cur) => {
+      if (gradeSum / personCount <= cur) {
+        successCount++;
+      }
+    });
+
+    console.log(`${successCount}/${personCount}`);
+  }
+  process.exit();
 })();
