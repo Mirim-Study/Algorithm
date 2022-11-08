dd3cfb0751800b39af23d49930d6719efd2dacee
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-11-08 21:17:14 +0900

    Update: 각 키의 차로 정렬되도록 리팩토링

--- goormedu/알고리즘{먼데이}/[2주차]-2.철자_분리_집합/areumsheep.js
+++ goormedu/알고리즘{먼데이}/[2주차]-2.철자_분리_집합/areumsheep.js
@@ -3,23 +3,20 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const data = [];
 
-rl.on('line', (line) => {
-	data.push(line);
-	
-}).on('close', () => {
-	console.log(solution(data));
-	process.exit();
-});
+rl.on('line', (line) => data.push(line))
+	.on('close', () => { console.log(solution(data)); process.exit(); });
 
 const solution = (data) => {
-	let count = 0;
-	let beforeWord = '';
-	const wordArray = data[1].toLowerCase().split('');
+	const [count, index] = data.shift().split(' ');
 	
-	wordArray.forEach((cur) => {
-		if(cur !== beforeWord) count++;
-		beforeWord = cur;
-	})
+	data.sort((a, b) => {
+		const [firstName, firstHeight] = a.split(' ');
+		const [secondName, secondHeight] = b.split(' ');
+		
+		if(firstName < secondName) return -1;
+		else if(firstName > secondName) return 1;
+		else return Number(firstHeight) - Number(secondHeight);
+	});
 	
-	return count;
+	return data[index-1];
 };
