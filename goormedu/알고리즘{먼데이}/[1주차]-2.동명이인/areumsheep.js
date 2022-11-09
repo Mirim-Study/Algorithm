efc749a4d8ade686f583b9d27e20e550af8a6cb8
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-11-09 16:48:52 +0900

    [1주차] 2.동명이인 (#517)
    * Create areumsheep.js
    
    * Rename 알고리즘{먼데이}/[1주차]-1.동명이인/areumsheep.js to goorm/알고리즘{먼데이}/[1주차]-1.동명이인/areumsheep.js
    
    * Rename goorm/알고리즘{먼데이}/[1주차]-1.동명이인/areumsheep.js to goorm/알고리즘{먼데이}/[1주차]-2.동명이인/areumsheep.js
    
    * Rename goorm/알고리즘{먼데이}/[1주차]-2.동명이인/areumsheep.js to goormedu/알고리즘{먼데이}/[1주차]-2.동명이인/areumsheep.js
    
    * Update: 정규식으로 비교하도록 변경
    
    * Update: `includes`로 이름 비교하는 구문으로 변경

---
+++ goormedu/알고리즘{먼데이}/[1주차]-2.동명이인/areumsheep.js
@@ -0,0 +1,25 @@
+// Run by Node.js
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+let data = [];
+
+rl.on('line', (line) => {
+	data.push(line);
+	
+}).on('close', () => {
+	console.log(solution(data));
+	process.exit();
+});
+
+const solution = (data) => {
+  let count = 0;
+  const firstLine = data.shift();
+  const [repeat, standardName] = firstLine.split(' ');
+
+  for (const name of data) {
+    if (name.includes(standardName)) count++;
+  }
+  return count;
+};
+
+console.log(solution(['4 fred', 'sam', 'fredy', 'fredi', 'ilium']));
