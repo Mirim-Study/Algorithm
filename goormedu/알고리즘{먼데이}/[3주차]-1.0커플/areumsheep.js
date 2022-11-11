185127dd221fa146ae6d2bb1cbb8376c8955a7f8
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-11-11 21:55:26 +0900

    Update areumsheep.js

--- goormedu/알고리즘{먼데이}/[3주차]-1.0커플/areumsheep.js
+++ goormedu/알고리즘{먼데이}/[3주차]-1.0커플/areumsheep.js
@@ -7,14 +7,7 @@ rl.on('line', (line) => data.push(line))
 	.on('close', () => { console.log(solution(data)); process.exit(); });
 
 const solution = (data) => {
-	const result = [];
-	const inputCount = data.shift();
-	const grades = data[0].split(' ').map(Number).sort((a,b) => b-a);
-	
-	for(let i = 0; i < grades.length; i++){
-		const minusIndex = grades.indexOf(-grades[i]);
-		if(minusIndex === -1) result.push(grades[i]);
-	}
-	
-	return result.reduce((acc, cur) => acc += cur);
+	data.shift();
+	const person = data[0].split(' ').map(Number);
+	return person.reduce((acc, cur) => acc += cur);
 };
